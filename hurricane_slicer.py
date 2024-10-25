14e8a79cb21278579fb17d7c1598189c7f5ced26
MitchieMoo <109352835+MHuott@users.noreply.github.com>
2024-10-25 14:29:50 -0400

    Add files via upload

--- hurricane_slicer.py
+++ hurricane_slicer.py
@@ -11,6 +11,8 @@ def hurricaneslicer(bt, myList, r):
 
     lonMid = bt.longitude.data[xMid]
     latMid = bt.latitude.data[yMid]
+    lonMin = bt.longitude.data[xMid - r]
+    latMin = bt.longitude.data[yMid - r]
 
     p1 = myList[math.floor(xMid + r), yMid]
     p2 = myList[math.floor(xMid - r), yMid]
@@ -23,4 +25,19 @@ def hurricaneslicer(bt, myList, r):
 
     pAvg = 0.125 * (p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8)
 
-    return pAvg
+    lat1 = math.radians(latMid)
+    lat2 = math.radians(latMin)
+
+    # This is the difference in longitude and latitude
+    dLat = lat2 - lat1
+    dLon = 0
+
+    # This value takes the difference in the dLon and dLat and
+    a = math.sin(dLat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(dLon / 2) ** 2
+    c = 2 * math.asin(math.sqrt(a))
+    earthRadius = 6371  # radius of earth in kilometers
+
+    radius = c * earthRadius
+
+
+    return pAvg, radius
