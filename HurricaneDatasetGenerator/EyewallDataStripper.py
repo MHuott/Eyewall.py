7f474271fe4b817bdef3faa32304bfebb09fc3ed
MitchieMoo <109352835+MHuott@users.noreply.github.com>
2026-02-02 09:33:17 -0500

    Add files via upload

--- HurricaneDatasetGenerator/EyewallDataStripper.py
+++ HurricaneDatasetGenerator/EyewallDataStripper.py
@@ -8,9 +8,12 @@ import numpy as np
 import math
 import xarray
 import matplotlib.pyplot as plt
-from EyewallGradient import eyewall_gradient
+from sympy.printing.pretty.pretty_symbology import line_width
 
-fp = "20201116T100000.nc"  #Break glass in case of emergency
+from EyewallGradient import eyewall_gradient
+from scipy.signal import savgol_filter
+
+#Break glass in case of emergency
 
 
 def filter(fp, count):
@@ -51,16 +54,35 @@ def filter(fp, count):
         btData[r] = result[0]
         radius[r] = result[1]
 
-    primaryIndex = eyewall_gradient(btData, radius, 0)
-    primaryBT = btData[primaryIndex]
+    New_btData = savgol_filter(btData, 8, 4)
+
+    primaryIndex = eyewall_gradient(New_btData, radius, 0)
+    primaryBT = New_btData[primaryIndex]
     primaryRadius = radius[primaryIndex]
 
 
-    secondaryIndex = eyewall_gradient(btData, radius, primaryIndex)
+    secondaryIndex = eyewall_gradient(New_btData, radius, primaryIndex)
 
 
-    secondaryBT = btData[secondaryIndex]
+    secondaryBT = New_btData[secondaryIndex]
     secondaryRadius = radius[secondaryIndex]
+
+    plot2 = plt.figure(figsize=(8, 6))
+    plt.plot(radius, btData, linestyle='--', color='red',linewidth = 3)
+    plt.plot(radius, New_btData, linewidth = 3)
+    plt.title("Hurricane Sam at 1145 on 11/04", fontsize=20)
+    plt.xlabel("Hurricane Radius (Km)", fontsize=18)
+    plt.ylabel("Brightness Temperature", fontsize=18)
+    plt.legend(['Raw Data', 'Filtered Data'], fontsize=15)
+    plt.tick_params(axis='both', labelsize=12)
+    plt.savefig("HurricaneSamSlice.png")
+    plt.show()
+
+    #print(primaryRadius)
 
 
     return primaryRadius, primaryBT, secondaryRadius, secondaryBT
+
+if __name__ == '__main__':
+    fp = '/Volumes/MHUOTT_PHYS/Hurricane Research/Tropical Cylone/Tropical Cyclone Data/2021 Season/2021_18L Sam/20211004T114500.nc'
+    filter(fp, 1)
